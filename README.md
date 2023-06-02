850d2290a9092fb723980d3a7ef3e13c621c3e8c
Max Hager <maxhager28@gmail.com>
2023-06-02 13:27:41 +0200

    added prompt file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 16personalities_chat
+
+
+## tasks
+
+- [ ] create channel for feature requests 
+- [ ] 
+
+
+what is the ideal outcome here? people go on discord and than they type "/chat" followed by the personality they want to chat with and than they get via dm a response from the personality type they wanna chat with. if they had in total triggered 5 messages from the bot they get a message that they first need to subscribe to further chat. they get forwarded to stripe where they have to pay 10€ for unlimited chats. can also add the function that they first would need to define the bot before start with chatting - type, age, gender and name 
+
+
+i just need one personality class and a script for dealing with discord. the rest is an ease. 
+
+once i have cash i also could join a college to get into the usa. i create the most advanced nsfw chat additionally. i also could try to raise money for scaling that once i got users. i want users!!!!!!!!!
--- personality.py
+++ personality.py
@@ -1,3 +1,50 @@
-#need to create 
+#need to create personality 
 
-#i created a midjourney generated photo with a description of each personality type from 16 personalities myers briggs. here is the result:
+#this is going to be so much fun lol.
+
+#i can contactc dozens of influencers to make advertisement for me. can scale rt
+#should be able to push out the mvp tonight 
+#can make a channel with feature requests
+
+
+'''
+self.messages = [
+            {
+                "role": "system",
+                "content": (
+                    "We are playing a chess game. At every turn, repeat all the moves that have already been made."
+                    "Find the best response for Black. I'm White and the game starts with 1.{first_move}\n\n"
+                    "So, to be clear, your output format should always be:\n\n"
+                    "PGN of game so far: ...\n\n"
+                    "Best move: ...\n\n"
+                    "and then I get to play my move. Do not include the move number'."
+                ),
+            }
+        ]
+        
+completion = openai.ChatCompletion.create(model=self.model, messages=messages)
+
+
+'''
+import json
+
+class Personality():
+    def __init__(self, name, age, gender, type, user_name, user_gender):
+        self.name = name
+        self.chat()
+        
+        
+    def chat(self):
+        #get the value from the json file prompts.json where the key is equals to self.name
+        with open('prompts.json') as f:
+            data = json.load(f)
+        value = data[self.name]
+        #insert at ht
+        print(value)
+        #defininbg openai chat here 
+        pass
+    
+    
+        
+        
+        
