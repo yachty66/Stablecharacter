5ee9e3af47c3f0e067b8c381fe534b3f988e0546
Max Hager <maxhager28@gmail.com>
2023-06-02 19:00:21 +0200

    slash command with polls

--- disord_bot.py
+++ disord_bot.py
@@ -11,8 +11,18 @@ bot = discord.Bot()
 )
 @option(
     "user_gender",
-    str,
-    description="Enter your gender"
+    int,
+    description="Choose your gender",
+    choices=[
+        discord.OptionChoice(name="Male", value=1),
+        discord.OptionChoice(name="Female", value=2)
+    ]
+)
+@option(
+    "user_age",
+    int,
+    description="Enter your age",
+    min_value=18
 )
 @option(
     "bot_name",
@@ -26,24 +36,49 @@ bot = discord.Bot()
 )
 @option(
     "bot_gender",
-    str,
-    description="Enter bot's gender"
+    int,
+    description="Choose bot's gender",
+    choices=[
+        discord.OptionChoice(name="Male", value=1),
+        discord.OptionChoice(name="Female", value=2)
+    ]
 )
 @option(
     "bot_personality",
     str,
-    description="Enter bot's personality"
+    description="Choose bot's personality",
+    choices=[
+        discord.OptionChoice(name="INTJ", value="INTJ"),
+        discord.OptionChoice(name="INTP", value="INTP"),
+        discord.OptionChoice(name="ENTJ", value="ENTJ"),
+        discord.OptionChoice(name="ENTP", value="ENTP"),
+        discord.OptionChoice(name="INFJ", value="INFJ"),
+        discord.OptionChoice(name="INFP", value="INFP"),
+        discord.OptionChoice(name="ENFJ", value="ENFJ"),
+        discord.OptionChoice(name="ENFP", value="ENFP"),
+        discord.OptionChoice(name="ISTJ", value="ISTJ"),
+        discord.OptionChoice(name="ISFJ", value="ISFJ"),
+        discord.OptionChoice(name="ESTJ", value="ESTJ"),
+        discord.OptionChoice(name="EFFJ", value="EFFJ"),
+        discord.OptionChoice(name="ISTP", value="ISTP"),
+        discord.OptionChoice(name="ISFP", value="ISFP"),
+        discord.OptionChoice(name="ESTP", value="ESTP"),
+        discord.OptionChoice(name="ESFP", value="ESFP")
+    ]
 )
 async def chat_command(
     ctx, 
     user_name: str,
-    user_gender: str,
+    user_gender: int,
+    user_age: int,
     bot_name: str,
     bot_age: int,
-    bot_gender: str,
+    bot_gender: int,
     bot_personality: str
 ):
-    await ctx.respond(f"Received your details! Name: {user_name}, Gender: {user_gender}. I am {bot_name}, a {bot_age} year old {bot_gender} bot with a {bot_personality} personality.")
+    user_gender_text = "Male" if user_gender == 1 else "Female"
+    bot_gender_text = "Male" if bot_gender == 1 else "Female"
+    await ctx.respond(f"Received your details! Name: {user_name}, Age: {user_age}, Gender: {user_gender_text}. I am {bot_name}, a {bot_age} year old {bot_gender_text} bot with a {bot_personality} personality.")
 
 
 bot.run("MTExMjU1NTYzMjU0NjA0MTg5Ng.G8oodP.f7rccXDaTjm_jYLJpNoj1XFfYGknIG4KN1UD8U")
