f9429087b909909206f7723cd25ee48e01fc0d14
Max Hager <maxhager28@gmail.com>
2023-06-05 23:58:38 +0200

    new chathistory implementation

--- discord_bot.py
+++ discord_bot.py
@@ -11,9 +11,9 @@ openai.api_key = "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"
 
 class ChatBot:
     def __init__(self):
-        self.bot_name_meta = "bot1112555632546041896"
+        self.mbti_name_meta = "bot1112555632546041896"
         self.is_processing = {}
-        self.bot_gender = ""
+        self.mbti_gender = ""
         self.bot_mbti = ""
         self.color = 0x000000
         self.register_commands()
@@ -52,43 +52,43 @@ class ChatBot:
             discord.OptionChoice(name="ESTP", value="ESTP"),
             discord.OptionChoice(name="ESFP", value="ESFP")
         ])
-        async def chat_command(ctx, user_name: str, user_gender: int, user_age: int, bot_name: str, bot_age: int, bot_gender: int, bot_personality: str):            
+        async def chat_command(ctx, user_name: str, user_gender: int, user_age: int, mbti_name: str, mbti_age: int, mbti_gender: int, mbti_personality: str):            
             if ctx.channel.name != "chat-room":
                 chat_room_channel = discord.utils.get(ctx.guild.channels, name="chat-room")
                 await ctx.respond(f"This command can only be used in the {chat_room_channel.mention} channel.")
                 return
-            if ',' in user_name or ',' in bot_name:
+            if ',' in user_name or ',' in mbti_name:
                 await ctx.respond("Commas are not allowed in user or bot names.")
                 return
             user_gender = "Male" if user_gender == 1 else "Female"
-            bot_gender = "Male" if bot_gender == 1 else "Female"
-            response_text = f"Name: {user_name}, Age: {user_age}, Gender: {user_gender}. I am {bot_name}, a {bot_age} year old {bot_gender} mbti with a {bot_personality} personality."
-            if "NT" in bot_personality:
+            mbti_gender = "Male" if mbti_gender == 1 else "Female"
+            response_text = f"Name: {user_name}, Age: {user_age}, Gender: {user_gender}. I am {mbti_name}, a {mbti_age} year old {mbti_gender} mbti with a {mbti_personality} personality."
+            if "NT" in mbti_personality:
                 self.color = 0x886199
-            elif "NF" in bot_personality:
+            elif "NF" in mbti_personality:
                 self.color = 0x32A474
-            elif "STP" in bot_personality or "SFP" in bot_personality:
+            elif "STP" in mbti_personality or "SFP" in mbti_personality:
                 self.color = 0xE4AE3A
             else:
                 self.color = 0x4198B4
-            embed = Embed(title=f"{bot_personality}-{bot_gender}", description=response_text, color=self.color)
+            embed = Embed(title=f"{mbti_personality}-{mbti_gender}", description=response_text, color=self.color)
             await ctx.respond(embed=embed)
             user_name = str(user_name)
             user_age = str(user_age)
             user_gender= str(user_gender)
-            bot_name = str(bot_name)
-            bot_age = str(bot_age)
-            bot_gender = str(bot_gender)
-            bot_personality = str(bot_personality)
+            mbti_name = str(mbti_name)
+            mbti_age = str(mbti_age)
+            mbti_gender = str(mbti_gender)
+            mbti_personality = str(mbti_personality)
             text_channel = ctx.channel
-            thread = await text_channel.create_thread(name=f"{bot_personality}-{bot_gender}", type=discord.ChannelType.private_thread)
+            thread = await text_channel.create_thread(name=f"{mbti_personality}-{mbti_gender}", type=discord.ChannelType.private_thread)
             await thread.add_user(ctx.author)
-            first_message = f"user_name: {user_name}, user_age: {user_age}, user_gender: {user_gender}, bot_name: {bot_name}, bot_age: {bot_age}, bot_gender: {bot_gender}, bot_personality: {bot_personality}"
+            first_message = f"user_name: {user_name}, user_age: {user_age}, user_gender: {user_gender}, mbti_name: {mbti_name}, mbti_age: {mbti_age}, mbti_gender: {mbti_gender}, mbti_personality: {mbti_personality}"
             await thread.send(first_message) 
             await thread.send(embed=embed)
     
     async def message_data(self, message):
-        chat_history = await message.channel.history().flatten()
+        chat_history = await message.channel.history().flatten()        
         second_oldest_messages = chat_history[-2:][0]
         message_parts = second_oldest_messages.content.split(', ')
         values = {}
@@ -98,33 +98,68 @@ class ChatBot:
         user_name = values['user_name']
         user_age = values['user_age']
         user_gender = values['user_gender']
-        bot_name = values['bot_name']
-        bot_age = values['bot_age']
-        bot_gender = values['bot_gender']
-        bot_personality = values['bot_personality']
+        mbti_name = values['mbti_name']
+        mbti_age = values['mbti_age']
+        mbti_gender = values['mbti_gender']
+        mbti_personality = values['mbti_personality']
         chat_list = []
-        for msg in chat_history:
-            chat_list.append({msg.author.name: msg.content})
-        return chat_history, user_name, user_age, user_gender, bot_name, bot_age, bot_gender, bot_personality
+        for msg in reversed(chat_history):
+            print("content")
+            if msg.embeds:
+                embed = msg.embeds[0]
+                content = embed.description
+            else:
+                content = msg.content
+            print(content)
+            chat_list.append({msg.author.name: content})
+        return chat_list, user_name, user_age, user_gender, mbti_name, mbti_age, mbti_gender, mbti_personality
                         
     async def message_response(self, message):
-        chat_history, user_name, user_age, user_gender, bot_name, bot_age, bot_gender, bot_personality = await self.message_data(message)
-        self.bot_gender = bot_gender
-        self.bot_mbti = bot_personality
+        chat_history, user_name, user_age, user_gender, mbti_name, mbti_age, mbti_gender, mbti_personality = await self.message_data(message)
+        self.mbti_gender = mbti_gender
+        self.bot_mbti = mbti_personality
         with open('prompts.json') as f:
             data = json.load(f)
-        value = data[bot_personality]
-        value = value.replace('{name}', bot_name)
-        value = value.replace('{age}', bot_age)
-        value = value.replace("{gender}", bot_gender)
-        value = value.replace("{type}", bot_personality)
+        value = data[mbti_personality]
+        value = value.replace('{name}', mbti_name)
+        value = value.replace('{age}', mbti_age)
+        value = value.replace("{gender}", mbti_gender)
+        value = value.replace("{type}", mbti_personality)
         value = value.replace("{user_name}", user_name)
         value = value.replace("{user_gender}", user_gender)
         messages = [
             {"role": "system", "content": value},
         ]
         chat_history = chat_history[:-3]
+        print("chat history -3")
+        print(chat_history)
+        #iter over 
         combined_chat_history = []
+        for msg in chat_history:
+            if combined_chat_history and list(msg.keys())[0] != "MBTI" and list(combined_chat_history[-1].keys())[0] != "MBTI":
+                combined_chat_history[-1][list(msg.keys())[0]] += f" {list(msg.values())[0]}"
+            else:
+                combined_chat_history.append(msg)
+                
+        for msg in combined_chat_history:
+            for author, content in msg.items():
+                role = "user" if author != "MBTI" else "assistant"
+                messages.append({"role": role, "content": content})
+
+        print("messages")
+        print(messages)
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        response = completion.choices[0].message["content"]
+        return response
+        
+        
+        
+        
+        
+        '''combined_chat_history = []
         for i, msg in enumerate(chat_history):
             if i > 0 and msg.author == chat_history[i - 1].author:
                 combined_chat_history[-1][msg.author.name] += f" {msg.content}"
@@ -135,14 +170,15 @@ class ChatBot:
             for author, content in msg.items():
                 role = "user" if author == user_name else "assistant"
                 messages.append({"role": role, "content": content})
-        print("messages: ")
-        print(messages)
+                
+                
+                
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages
         )
         response = completion.choices[0].message["content"]
-        return response
+        return response'''
 
     def register_events(self):
         @bot.event
@@ -152,18 +188,16 @@ class ChatBot:
             user_id = message.author.id
             if user_id not in self.is_processing:
                 self.is_processing[user_id] = False
-            
             if self.is_processing[user_id]:   
                 return
-            
             if message.content.strip() != "":
                 self.is_processing[user_id] = True
                 response = await self.message_response(message)
                 embed_response = Embed(description=response, color=self.color)
-                image_url = f"images_{self.bot_gender}s/{self.bot_mbti}.png"
+                image_url = f"images_{self.mbti_gender}s/{self.bot_mbti}.png"
                 with open("image_map.json") as img_map_file:
                     image_map = json.load(img_map_file)
-                image_key = self.bot_mbti + "_" + self.bot_gender
+                image_key = self.bot_mbti + "_" + self.mbti_gender
                 image_url = image_map[image_key]
                 embed_response.set_image(url=image_url)  
                 await message.channel.send(embed=embed_response)
