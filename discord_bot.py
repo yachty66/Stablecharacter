d41e9f5b9e7133cbf6b38a994dd82f711a8058c0
Max Hager <maxhager28@gmail.com>
2023-06-06 14:33:49 +0200

    only allow messages which are inside a thread

--- discord_bot.py
+++ discord_bot.py
@@ -147,11 +147,17 @@ class ChatBot:
         response = completion.choices[0].message["content"]
         return response
 
+
     def register_events(self):
         @bot.event
         async def on_message(message):            
             if message.author == bot.user:
                 return
+            print("message received in register event")
+            if not isinstance(message.channel, discord.Thread):
+                print("message is not in thread")
+                return
+            
             user_id = message.author.id
             if user_id not in self.is_processing:
                 self.is_processing[user_id] = False
@@ -159,6 +165,7 @@ class ChatBot:
                 return
             if message.content.strip() != "":
                 self.is_processing[user_id] = True
+                print("message will be processes")
                 response = await self.message_response(message)
                 embed_response = Embed(description=response, color=self.color)
                 image_url = f"images_{self.mbti_gender}s/{self.bot_mbti}.png"
