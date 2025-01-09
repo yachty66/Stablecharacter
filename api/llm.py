64f5e3e36c8faccdb1884a3d1af3a70614696ff1
Max Hager <maxhager28@gmail.com>
2025-01-09 01:06:32 -0800

    added example authors note injection in llm.py

--- api/llm.py
+++ api/llm.py
@@ -17,5 +17,12 @@ def call_llm(model="gemini/gemini-2.0-flash-exp", messages=[], max_tokens=None, 
 
 
 if __name__ == "__main__":
-    response = call_llm(messages=[{"role": "user", "content": "write code for saying hi from LiteLLM"}])
-    print(response)
+    response = call_llm(messages=[{"role": "system", "content": "You are an intp from the 16 personalities test."}, 
+                                  {"role": "user", "content": "hey dude"},
+                                  {"role": "assistant", "content": "hey."},
+                                  {"role": "system", "content": "the following authors note is injected into the situation: *youre inside inside a room with no objects in it, only white walls*"},
+                                  {"role": "user", "content": "how is your day going? and what is your type? and how would you explain how the room we are in currently looks like?"},
+                                  ],
+                                  temperature=0.0,
+                                  )
+    print(response.choices[0].message.content)
