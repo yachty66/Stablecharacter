b782876e4bb123c6dec30b712f36bb65988e6f6c
Max Hager <68147541+yachty66@users.noreply.github.com>
2025-01-24 16:53:47 -0800

    sitemap works

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,11 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://yourdomain.com';
-  
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: 'https://stablecharacter.com/sitemap.xml',
   };
 }
